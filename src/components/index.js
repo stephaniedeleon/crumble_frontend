d5dea1a01fee9d2ac3dff2937cb93102b0f9de83
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-07-09 09:38:33 -0700

    updated from main

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export {default as Navbar } from "./Navbar/Navbar";
+
+export {default as MainTab } from "./MainTab/MainTab";
+export {default as SubTab } from "./SubTab/SubTab";
+
+export {default as Notes } from "./Notes/Notes";
+export {default as Task } from "./Task/Task";
+export {default as ToDo } from "./ToDo/ToDo";;
