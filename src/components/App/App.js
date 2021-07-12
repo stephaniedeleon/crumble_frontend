4a807aa0f1eb1717794decbc60e958a157328612
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-07-12 09:38:40 -0700

    imports basic requirements for react-bootstrap

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 
 import React from "react";
+import { Navbar } from "components"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      
+      <Navbar />
+
       <Routes>
         <Route path="/" />
         <Route path="/login" />
