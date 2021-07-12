74732468f8e3eff0882b2d15d81c8bc01933af05
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-07-12 16:39:27 -0700

    creates useLogin hook

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,60 +1,42 @@
-import "./Login.css"
+import "./Login.css";
 
-import { Form, FormGroup, FormLabel, Button, Container } from "react-bootstrap"
-import { useEffect, useState } from "react"
-import { Navigate, useNavigate } from "react-router"
+import { Form, FormGroup, FormLabel, Button, Container } from "react-bootstrap";
+import { useLogin } from "hooks/useLogin";
 
+export default function Login() {
+  const { form, handleOnTextChange, handleOnClickSubmit } = useLogin();
 
-export default function Login ({ user, setUser }) {
+  return (
+    <div className="Login">
+      <Container className="px-5">
+        <Form onSubmit={handleOnClickSubmit}>
+          <FormGroup>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter Email"
+              onChange={handleOnTextChange}
+              value={form.email}
+              required
+            />
+          </FormGroup>
 
-    const navigate = useNavigate()
-    const [isProcessing, setIsProcessing] = useState(false)
-    const [errors, setErrors] = useState({})
-    const [form, setForm] = useState({
-        email: "",
-        password: "",
-    })
+          <FormGroup>
+            <FormLabel>Password</FormLabel>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+              onChange={handleOnTextChange}
+              value={form.password}
+              required
+            />
+          </FormGroup>
 
-    useEffect(() => {
-        //if user logged in, take to homepage
-        if (user?.email) {
-            navigate("/home")
-        }
-    }, [user, navigate])
-
-    const handleOnTextChange = (evt) => {
-        if (evt.target.name === "email") {
-            if (evt.target.value.indexOf("@") <= 0)
-                setErrors((err) => ({ ...err, email: "Please enter a valid email."}))
-            else 
-                setErrors((err) => ({ ...err, email: null}))
-        }
-
-        setForm((oldForm) => ({ ...oldForm, [evt.target.name]: evt.target.value }))
-    }
-
-
-
-    return (
-        <div className="Login">
-            <Container className="px-5">
-                <Form onSubmit={handleOnClickSubmit}>
-                    <FormGroup>
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" name="email" placeholder="Enter Email" onChange={handleOnTextChange} value={form.email} required />
-                    </FormGroup>
-
-                    <FormGroup>
-                        <FormLabel>Password</FormLabel>
-                        <Form.Control type="password" name="password" placeholder="Enter Password" onChange={handleOnTextChange} value={form.password} required />
-                    </FormGroup>
-
-                    <Button type="submit">
-                        Register
-                    </Button>
-                    
-                </Form>
-            </Container>
-        </div>
-    )
+          <Button type="submit">Register</Button>
+        </Form>
+      </Container>
+    </div>
+  );
 }
---
+++ src/hooks/useLogin.js
@@ -0,0 +1,65 @@
+import { useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router"
+import apiClient from "services/apiClient"
+import AuthContext from "context/auth"
+
+
+export const useLogin = () => {
+    const navigate = useNavigate()
+    const { user, setUser } = useContext(AuthContext)
+    const [isProcessing, setIsProcessing] = useState(false)
+    const [errors, setErrors] = useState({})
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+    })
+
+    useEffect(() => {
+        //if user logged in, take to homepage
+        if (user?.email) {
+            navigate("/home")
+        }
+    }, [user, navigate])
+
+    const handleOnTextChange = (evt) => {
+        if (evt.target.name === "email") {
+            if (evt.target.value.indexOf("@") <= 0)
+                setErrors((err) => ({ ...err, email: "Please enter a valid email."}))
+            else 
+                setErrors((err) => ({ ...err, email: null}))
+        }
+
+        setForm((oldForm) => ({ ...oldForm, [evt.target.name]: evt.target.value }))
+    }
+
+    const handleOnClickSubmit = async (evt) => {
+        evt.preventDefault()
+        setIsProcessing(true)
+        setErrors((err) => ({ ...err, form: null }))
+
+        const { data, error } = await apiClient.login({
+            email: form.email,
+            password: form.password
+        })
+
+        if (error) {
+            setErrors((err) => ({ ...err, form: error }))
+        }
+
+        if (data) {
+            setUser(data.user)
+            apiClient.setToken(data.token)
+            localStorage.setItem("token", data.token)
+        }
+
+        setIsProcessing(false)
+    }
+
+    return {
+        isProcessing,
+        errors,
+        form,
+        handleOnTextChange,
+        handleOnClickSubmit,
+    }
+}
