98d1021d9aa848bac6e11d5d0af292521d756030
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-07-15 13:04:43 -0700

    links maintab to tabpage

--- src/components/Pages/TabPage/TabPage.js
+++ src/components/Pages/TabPage/TabPage.js
@@ -1,11 +1,47 @@
 import "./TabPage.css"
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import apiClient from "services/apiClient";
 
 
-export default function TabPage () {
+export default function TabPage() {
+
+    const { mainId } = useParams();
+    const [tab, setTab] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+
+    //Getting maintab details...
+    useEffect(() => {
+
+        const fetchMainTabById = async() => {
+
+            setIsLoading(true);
+
+            try {
+                const { data } = await apiClient.getMaintab(mainId)
+
+                if (data?.maintab) {
+                    setTab(data.maintab);
+                } else {
+                    setError("Tab not found");
+                }
+
+            } catch (err) {
+                    console.log({err})
+            }
+            
+            setIsLoading(false);    
+        }
+
+        fetchMainTabById();
+
+    }, [mainId])
 
     return (
         <div className="TabPage">
-
+            <h1>TAB PAGE - {tab.name}</h1>
         </div>
     );
 }
