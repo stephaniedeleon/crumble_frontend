3638c3d69cac71aafab2f709ba7f2a11266d0d91
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-07-15 13:35:00 -0700

    clean up code for sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,44 +1,37 @@
 import "./SideBar.css";
 
 import { OffCanvas, OffCanvasMenu } from "react-offcanvas";
-import { useState } from "react";
 import { Button, Col, Row } from "react-bootstrap";
-
+import { useSideBar } from "hooks/useSideBar";
 
 export default function SideBar() {
-
-    const width = 300
-    const [isMenuOpened, setIsMenuOpened] = useState(false)
-    
-    const handleClick = () => {
-        // toggles the menu opened state
-        setIsMenuOpened((isMenuOpened) => !isMenuOpened)
-    }
-
+  const { width, isMenuOpened, handleClick } = useSideBar();
 
   return (
-    <div className="SideBar" style={{width: `${width}px`}}>
-            <Row className="mr-0">
-                <Col className="menuBtn pr-0">
-                    <Button onClick={handleClick}>Click</Button>
-                </Col>
-            </Row>
-            <Row className="menuContent  mr-0">
-                <Col className="pr-0">
-                    <OffCanvas
-                        width={width}
-                        transitionDuration={300}
-                        isMenuOpened={isMenuOpened}
-                        position={"left"}
-                        effect={"overlay"}
-                    >
-                        <OffCanvasMenu className={"my-menu-class"} style={{ fontWeight: "bold", position: "relative" }}>
-                            This is the canvas menu.
-                        </OffCanvasMenu>
-
-                    </OffCanvas>
-                </Col>
-            </Row>
+    <div className="SideBar" style={{ width: `${width}px` }}>
+      <Row className="mr-0">
+        <Col className="menuBtn pr-0">
+          <Button onClick={handleClick}>Click</Button>
+        </Col>
+      </Row>
+      <Row className="menuContent  mr-0">
+        <Col className="pr-0">
+          <OffCanvas
+            width={width}
+            transitionDuration={300}
+            isMenuOpened={isMenuOpened}
+            position={"left"}
+            effect={"overlay"}
+          >
+            <OffCanvasMenu
+              className={"my-menu-class"}
+              style={{ fontWeight: "bold", position: "relative" }}
+            >
+              This is the canvas menu.
+            </OffCanvasMenu>
+          </OffCanvas>
+        </Col>
+      </Row>
     </div>
   );
 }
--- src/hooks/useSideBar.js
+++ src/hooks/useSideBar.js
@@ -2,5 +2,17 @@ import { useState } from "react";
 
 
 export const useSideBar = () => {
+    const width = 300
+    const [isMenuOpened, setIsMenuOpened] = useState(false)
+    
+    const handleClick = () => {
+        // toggles the menu opened state
+        setIsMenuOpened((isMenuOpened) => !isMenuOpened)
+    }
 
+    return {
+        width,
+        isMenuOpened,
+        handleClick,
+    }
 }
