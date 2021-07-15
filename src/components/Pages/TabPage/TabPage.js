d9ae515df5637f28c81613810f4ba405d584afef
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-07-15 13:33:08 -0700

    creates basic sidebar

--- src/components/Pages/TabPage/TabPage.js
+++ src/components/Pages/TabPage/TabPage.js
@@ -1,11 +1,12 @@
 import "./TabPage.css"
 
+import { SideBar } from "components";
 
 export default function TabPage () {
 
     return (
         <div className="TabPage">
-
+            <SideBar />
         </div>
     );
 }
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,11 +1,44 @@
-import "./SideBar.css"
+import "./SideBar.css";
+
+import { OffCanvas, OffCanvasMenu } from "react-offcanvas";
+import { useState } from "react";
+import { Button, Col, Row } from "react-bootstrap";
 
 
 export default function SideBar() {
 
-    return (
-        <div className="SideBar">
+    const width = 300
+    const [isMenuOpened, setIsMenuOpened] = useState(false)
+    
+    const handleClick = () => {
+        // toggles the menu opened state
+        setIsMenuOpened((isMenuOpened) => !isMenuOpened)
+    }
 
-        </div>
-    )
+
+  return (
+    <div className="SideBar" style={{width: `${width}px`}}>
+            <Row className="mr-0">
+                <Col className="menuBtn pr-0">
+                    <Button onClick={handleClick}>Click</Button>
+                </Col>
+            </Row>
+            <Row className="menuContent  mr-0">
+                <Col className="pr-0">
+                    <OffCanvas
+                        width={width}
+                        transitionDuration={300}
+                        isMenuOpened={isMenuOpened}
+                        position={"left"}
+                        effect={"overlay"}
+                    >
+                        <OffCanvasMenu className={"my-menu-class"} style={{ fontWeight: "bold", position: "relative" }}>
+                            This is the canvas menu.
+                        </OffCanvasMenu>
+
+                    </OffCanvas>
+                </Col>
+            </Row>
+    </div>
+  );
 }
