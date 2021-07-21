a651cef37d0ced9c57066988e15f60627c3010f6
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-07-21 08:36:12 -0700

    updated css

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,6 +1,6 @@
 import "./Home.css";
 
-import { Button } from "react-bootstrap";
+import { Button, Carousel } from "react-bootstrap";
 import { PageHeader, MainTab, AddMainTab } from "components";
 import React, { useContext, useState, useEffect } from "react";
 import AuthContext from "context/auth";
@@ -41,7 +41,43 @@ export default function Home() {
 
     return (
         <div className="Home">
-            <PageHeader sectionName={welcome}/>
+            <Carousel className="carousel">
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://images.unsplash.com/flagged/photo-1574512468809-d05a4fd197fd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                        alt="productivity pic"
+                    />
+                    <Carousel.Caption className="carousel-caption">
+                        <h3>{welcome}</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                {/* <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=Second slide&bg=282c34"
+                    alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=Third slide&bg=20232a"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item> */}
+            </Carousel>
 
             <div className="home-area">
                 <div className="title">
