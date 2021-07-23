9c3d797bf0e03b75830da68b11ded36687b481e0
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-07-23 08:57:59 -0700

    added deleting tasks and subtabs

--- src/components/SubTab/SubTab.js
+++ src/components/SubTab/SubTab.js
@@ -5,6 +5,7 @@ import { InputGroup, FormControl } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import AuthContext from "context/auth";
 import { useContext } from "react";
+import { DeleteSubtab } from "components";
 import apiClient from "services/apiClient";
  
 export default function SubTab(props) {
@@ -21,12 +22,28 @@ export default function SubTab(props) {
 
     const { digIntoTab } = useContext(AuthContext);
 
+    //method to show modal for deleting confirmation...
+    const [modalShow, setModalShow] = useState(false);
+    const subtab = props.subtab;
+
     return (
         <div className="SubTab">
             <InputGroup className="mb-3">
-                <InputGroup.Checkbox id="checkbox" checked={completed} onChange={handleChange} />
-                <FormControl id="subtabName" as={Link} to={`/home/${props.mainId}/${props.subtab.id}`} onClick={() => digIntoTab(props.subtab.id)} > {props.subtab.name} </FormControl>
+                <div className="details">
+                    <InputGroup.Checkbox id="checkbox" checked={completed} onChange={handleChange} />
+                    <FormControl id="subtabName" as={Link} to={`/home/${props.mainId}/${subtab.id}`} onClick={() => digIntoTab(subtab.id)} > {subtab.name} </FormControl>
+                </div>
+
+                <div className="delete">
+                    <i class="bi-x" onClick={() => setModalShow(true)}></i>
+                </div>
             </InputGroup>
+
+            <DeleteSubtab
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                subtab={subtab}
+            />
         </div>
     );
 } 
